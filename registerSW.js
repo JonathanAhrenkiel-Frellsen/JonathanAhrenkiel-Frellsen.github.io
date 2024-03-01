2e1a354b1dd8fe3e060193e2d86cb57011889ca3
JAF <jaf@based.com>
2024-03-01 18:07:24 +0100

    Updates

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/cv/sw.js', { scope: '/cv/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
