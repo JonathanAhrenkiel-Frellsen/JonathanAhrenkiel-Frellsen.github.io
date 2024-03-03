58d99d2bc3370d267cd6097de68e7e4c97168569
JAF <jaf@based.com>
2024-03-03 22:09:27 +0100

    removed react router dom

--- src/main.tsx
+++ src/main.tsx
@@ -1,21 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import GitProfile from './pages/gitprofile.tsx';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <GitProfile config={CONFIG} />,
-  },
-  // {
-  //   path: '*',
-  //   // element: <Page404 />,
-  // },
-]);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <GitProfile config={CONFIG} />
   </React.StrictMode>,
 );
